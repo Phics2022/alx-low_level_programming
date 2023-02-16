13b61ea7387dafa409f6e523c02fb2a28d8d8050
Oladipo <phaymos2022@gmail.com>
2023-02-16 13:05:14 -0800

    sizeof

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 /**
 *main - print using puts
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
