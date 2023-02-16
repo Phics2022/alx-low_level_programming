236ceef98442d9cc7bdf4070067b23581111a5ce
Oladipo <phaymos2022@gmail.com>
2023-02-16 10:01:28 -0800

    clng

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main-print usin printf
+ *printf prints some line
+ *
+ *Return: 0 if null
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art'");
+return (0);
+}
