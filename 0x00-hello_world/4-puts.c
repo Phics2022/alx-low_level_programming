7e1a165a6f91cea341d64c5df136a35755c1e4ad
Oladipo <phaymos2022@gmail.com>
2023-02-16 09:37:10 -0800

    clng

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*Print using pits*/
+/* main function print word with puts*/
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
- return (0);
+return (0);
 }
