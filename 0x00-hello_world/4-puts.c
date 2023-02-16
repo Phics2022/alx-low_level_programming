41b2638ca8de8777ac2026e9f1f1b11fac9ffe24
Oladipo <phaymos2022@gmail.com>
2023-02-16 09:34:23 -0800

    clng

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+/*Print using pits*/
 
-int main()
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+ return (0);
 }
