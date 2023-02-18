d66b95e5d013c25167c7b33d1d5faeaecae98542
Oladipo <phaymos2022@gmail.com>
2023-02-18 05:49:00 -0800

    clang

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* print numbers */
+/**
+ *main - print the numbers in a loop
+ *using putchar
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+/*print number using putchar*/
+ 
+for (i = 0; i < 10; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
