63b2cf3b781e34d6c35cc4e8e64998dbb5b28f95
Oladipo <phaymos2022@gmail.com>
2023-02-18 02:16:45 -0800

    var,int,if,while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*print lower case but no q or e*/
+/**
+ * main - holding the func for loop
+ *for prints the lowercase using continur=e to skip q and e
+ *
+ *return null
+ */
+int main(void)
+{
+  char c;
+  /* create lower case loop*/
+for (c = 'a'; c <= 'z'; c++)
+{
+  /* skip q snd e*/
+if (c == q || c == e)
+continue ;
+}
+}
