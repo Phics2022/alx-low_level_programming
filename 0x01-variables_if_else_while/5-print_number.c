12ec38da9b8d3bb3612bb81693f24951dd0540ba
Oladipo <phaymos2022@gmail.com>
2023-02-18 05:33:47 -0800

    clang

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*print numbers*/
+/**
+ *main - print out numbers
+ *using loops to generate number 0 to 9
+ *
+ *Return: 0
+ */
+int main(void)
+{
+
+int i;
+/* print 0 to 9*/
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
