1a4e0f197abe46c55b9a9830afd5a336e3e0986b
Oladipo <phaymos2022@gmail.com>
2023-02-17 17:58:38 -0800

    var,int,if,while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* print alphabet */
+/**
+ *main - print alphabets in a loop
+ *using a range of a to z
+ *
+ *Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z' ; ++c)
+putchar(c);
+return (0);
+}
