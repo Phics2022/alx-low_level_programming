a758f4f1f2baaf0d4d94548f05fa3173354eff4d
Oladipo <phaymos2022@gmail.com>
2023-02-17 16:33:48 -0800

    negative or positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* Positive or Negative */
+/**
+ *main- check if int is +ve or -ve
+ *if satement is used to check it
+ *
+ *Return: 0 if null
+ */
+int main(void)
+{
+  int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+ 
+ if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
