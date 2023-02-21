710030ad4570204e7aef8d9e4769bad8ed5af0b6
Oladipo <phaymos2022@gmail.com>
2023-02-21 11:49:12 -0800

    function_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main - write the alphabet
+ *putchar
+ *
+ *Return: 0
+ */
+int main(void)
+{
+print_alphabet("abcdefghijklmnop");
+_putchar(print_alphabet());
+return (0);
+}
