74c4fab42aaeb0b0f3ecc4b1016a2319c2f812e5
Oladipo <phaymos2022@gmail.com>
2023-02-22 07:12:47 -0800

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isalpha - checks if alphabet
+ *@c: parameter
+ *
+ *Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+if (c >= 65 && C <= 122)
+return (1);
+else
+return (0);
+}
