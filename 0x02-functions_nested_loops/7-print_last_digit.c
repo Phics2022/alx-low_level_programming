78b5ec326953dc14dc20e37bafe62b30206fbac1
Oladipo <phaymos2022@gmail.com>
2023-02-22 07:47:54 -0800

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_last_digit - print the last digit
+ *@p: parameter
+ *
+ *Return: 0
+ */
+int print_last_digit(int p)
+{
+int k = p % 10;
+if ( k < 0)
+k = -k;
+_putchar(k);
+return (k);
+}
