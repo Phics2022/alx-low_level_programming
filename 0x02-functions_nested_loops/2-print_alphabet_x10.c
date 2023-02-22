484904d6eb414e233e731f43ad51866598c3260e
Oladipo <phaymos2022@gmail.com>
2023-02-22 01:57:28 -0800

    functions nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  *_putchar - print 10 times
- *print_alphabet
+ *print_alphabet_x10 to putchar
  *
  *Return: 0;
  */
 void print_alphabet_x10(void)
 {
-  char c;
-  int i;
+char c;
+int i;
 for (i = 1; i <= 10; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
