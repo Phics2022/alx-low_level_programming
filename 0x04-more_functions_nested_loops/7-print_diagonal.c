7a6ad8d37e7c9a25af5a23c202f1f985ecb183e1
Oladipo <phaymos2022@gmail.com>
2023-02-25 08:02:17 -0800

    funcand nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,18 @@
  */
 void print_diagonal(int n)
 {   
-if (n >=1)
+if (n >= 1)
 {
-for(int y = 0; y < n; y++)
+for (int y = 0; y < n; y++)
 {
-for(int z = 0; z < n; z++)
+for (int z = 0; z < n; z++)
 {
-if(z == y)
+if (z == y)
 _putchar('\\');
 else
+{
 _putchar(' ');
+}
 }
 _putchar('\n');
 }
