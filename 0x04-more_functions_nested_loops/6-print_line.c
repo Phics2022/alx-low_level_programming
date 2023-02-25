5cb4d2806cd969c8f80adf4c5909c1ac26aeaa51
Oladipo <phaymos2022@gmail.com>
2023-02-25 04:25:34 -0800

    funcand nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,9 +7,15 @@
  */
 void print_line(int n)
 {
+if (n => 1)
+{
 int j,y;
 y = n + 1;
-for (j = 1; j < y j++)
-_putchar('-');
+for (j = 1; j < y; j++)
+_putchar('_');
+_putchar('\n');
+}
+else
+{
 _putchar('\n');
 }
