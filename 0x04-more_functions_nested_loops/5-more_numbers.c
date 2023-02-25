356ef76f9d1c9b7201c2779733032e58d81a9e2d
Oladipo <phaymos2022@gmail.com>
2023-02-25 05:05:49 -0800

    funcand nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,17 @@ int i, j;
 for (i = 0; i <= 10; i++)
 {
 for (j = 0; j <= 14; j++)
-_putchar('0' + j);
+{
+if (j < 10)
+{
+_putchar(j + '0');
+}
+else if (j >= 10)
+{
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+}
+}
 _putchar('\n');
 }
 }
