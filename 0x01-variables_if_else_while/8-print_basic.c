472d762bb74ee41a0c3261f21bf2a7622aad841d
Oladipo <phaymos2022@gmail.com>
2023-02-25 03:17:23 -0800

    func

---
+++ 0x01-variables_if_else_while/8-print_basic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*print in base 16*/
+/**
+ *main - print in base 6
+ * using putchar and %x specifier
+ *
+ *Return: 0
+ */
+int main(void)
+{
+  int i;
+  
+  putchar('\n');
+      return (0);
+}
