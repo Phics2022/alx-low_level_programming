3688abff58ad0dcefefe17099cc4ec7d3105e8db
Oladipo <phaymos2022@gmail.com>
2023-02-28 03:48:22 -0800

    arrays, pointers and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *swap_int - swap intergers
+ *@a: parameter
+ *@b: paameter
+ *
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
