b508dbbd2f730a6836c07801edc1b58552b6949a
Oladipo <phaymos2022@gmail.com>
2023-02-28 02:43:40 -0800

    git

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *void reset_to_98 - changr n to 98 using pounter o
+ *@n: parameter to change value
+ *
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+int *p;
+p = &n;
+*p = 98;
+}
