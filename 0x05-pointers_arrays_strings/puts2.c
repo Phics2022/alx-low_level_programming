7f2bc8c9d38d9b2bf582479bcae247e95b0bedf4
Oladipo <phaymos2022@gmail.com>
2023-03-01 05:33:07 -0800

    pas

---
+++ 0x05-pointers_arrays_strings/puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *put2 - print two forward
+ *@str: parameter
+ *
+ *Return: void
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i+=2)
+_putchar(str[i]);
+}
