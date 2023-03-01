b227f52c37946f1d5af99b24b3a10b73564340be
Oladipo <phaymos2022@gmail.com>
2023-03-01 05:51:18 -0800

    pas

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *puts_half - print half of the string
+ *@str: parameter
+ *
+ *Return: void
+ */
+void puts_half(char *str)
+{
+int i, j;
+for (i = 0; str[i] != '\0'; i++)
+;
+j = i / 2;
+while (str[j] != NULL)
+{
+putchar(str[j]);
+j++;
+}
