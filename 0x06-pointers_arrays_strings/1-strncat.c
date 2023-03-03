824c3e639e748c4a938509331de0f662739d0a2b
Oladipo <phaymos2022@gmail.com>
2023-03-03 01:35:01 -0800

    more

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 int i, j;
 for (i = 0; dest[i] != '\0'; i++)
 ;
-for (j = 0; j <= n - 1; j++)
+for (j = 0; j < n && src[j] != '\0'; j++)
 {
 dest[i] = src[j];
 i++;
