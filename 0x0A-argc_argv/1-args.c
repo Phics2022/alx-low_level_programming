20f7ef017d08ae06d7f7237ed4167c7a92fa1657
Oladipo <phaymos2022@gmail.com>
2023-03-12 10:00:03 -0700

    git

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-/**                                         
- *main - print my name                                          
- *@argc: counter                                             
- *@argv: vector                                             
- *                                                            
- *Return: void                                              
+/**
+ *main - print my name
+ *@argc: counter
+ *@argv: vector
+ *
+ *Return: void
  */
 int main(int argc, char  *argv[] __attribute__((unused)))
 {
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
 return (0);
 }
