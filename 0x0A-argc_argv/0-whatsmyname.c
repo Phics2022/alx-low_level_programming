a53532b72503ea6f2795c60377553ac6c1cfb622
Oladipo <phaymos2022@gmail.com>
2023-03-12 09:21:25 -0700

    git

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 /**
  *main - print my name
- *@argc: counter
  *@argv: vector
  *
  *Return: void
  */
-int main(int argc, char  *argv[]__attribute__((unused)))
+int main(char  *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
