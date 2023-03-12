880039eb919dad54482e0634a66170ad16c5d0bd
Oladipo <phaymos2022@gmail.com>
2023-03-12 11:02:59 -0700

    git

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - print my name
+ *@argc: counter
+ *@argv: vector
+ *
+ *Return: void
+ */
+int main(int argc, char  *argv[])
+{
+int mul, n;
+n = 1;
+mul = 1;
+if (argc > 1)
+{
+while (n < argc)
+{
+mul *= atoi(argv[n]);
+n++;
+}
+printf("%d\n", mul);
+return (0);
+}
+else
+{
+printf("Error");
+return (1);
+}
+}
