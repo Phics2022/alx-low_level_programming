97593caf369bf2b4b559d9d7138c0d442db09442
Oladipo <phaymos2022@gmail.com>
2023-03-14 02:29:35 -0700

    git

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*create_array - creat array
+*@size: size needed
+*@c: character
+*
+*Return: array
+*/
+char *create_array(unsigned int size, char c)
+{
+char *array;
+int i;
+if (size == 0)
+return (NULL);
+else
+{
+array = (char *) malloc(sizeof(char) * size);
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return (array);
+}
+}
