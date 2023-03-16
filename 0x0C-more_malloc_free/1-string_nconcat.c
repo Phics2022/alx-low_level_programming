83913566086e0827d0f6258d587a3836855d4ba6
Oladipo <phaymos2022@gmail.com>
2023-03-16 11:35:07 -0700

    git

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,14 +14,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
 int i, j;
+int x, y;
 j = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-if (n > strlen(s2))
-n = strlen(s2);
-s = malloc(strlen(s1) + n + 1);
+for (x = 0; s1[x]; x++)
+;
+for (y = 0; s2[y]; y++)
+;
+if (n > y)
+n = y;
+s = malloc(sizeof(char)* (x + n + 1));
 if (s == NULL)
 return (NULL);
 for (i = 0; s1[i]; i++)
