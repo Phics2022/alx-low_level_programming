8dc3d3b1b7960e4da04700ded2192d7a623aa688
Oladipo <phaymos2022@gmail.com>
2023-03-17 05:22:37 -0700

    git

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_calloc - allocate memory for an array
+ *@nmemb: member
+ *@size: size
+ *
+ *Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ptr;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+return (NULL);
+for (i = 0; i < (nmemb * size); i++)
+{
+ptr[i] = 0;
+}
+return (ptr);
+}
