96415bfaa260606bb784c54685ca5eb009590359
Oladipo <phaymos2022@gmail.com>
2023-03-21 02:39:46 -0700

    dog structuure with better style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ *struct dog - create structure for dog
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ *
+ *Description: create a structure with name dog and parameters of the following
+ */
 struct dog
 {
 char *name;
