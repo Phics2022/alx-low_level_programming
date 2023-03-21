d141ac402c80d5dfb7864327410bc6b0d21536f4
Oladipo <phaymos2022@gmail.com>
2023-03-21 12:33:17 -0700

    i freed the dogs i am a good man

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_dog - free a dog
+ *@d: dog to be freed
+ *
+ *Return: Void
+ */
+void free_dog(dog_t *d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
