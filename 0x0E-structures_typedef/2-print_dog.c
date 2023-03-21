760bb7a4cfbc08b392af90b450e32ce67eb323f7
Oladipo <phaymos2022@gmail.com>
2023-03-21 05:13:36 -0700

    git

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,15 +14,15 @@ else
 {
 if (d->name == NULL)
 printf("Name: nil\n");
-else 
+else
 printf("Name: %s\n", d->name);
 if (d->age == 0)
 printf("Age: nil\n");
-else 
+else
 printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: nil\n");
-else 
+else
 printf("Owner: %s\n", d->owner);
 }
 }
