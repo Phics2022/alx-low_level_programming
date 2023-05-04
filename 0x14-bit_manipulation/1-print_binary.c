92b400e778bde067883ddd561752232b9bafe32c
Oladipo <phaymos2022@gmail.com>
2023-05-04 09:53:03 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,13 +9,21 @@
  */
 void print_binary(unsigned long int n)
 {
-int mask = 1UL << sizeof(unsigned long int);
+unsigned long int mask = 1UL << sizeof(unsigned long int);
+int one = 0;
+while (mask <= n)
+	mask <<= 1;
 while (mask > 0)
 {
 	if (mask & n)
+	{
 		_putchar('1');
-	else
+		one = 1;
+	}
+	else if (one)
 		_putchar('0');
 	mask >>= 1;
 }
+if (!one)
+	_putchar ('0');
 }
