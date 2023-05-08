058ddf9199dfa191a8b1d7b117a8108b2c2876b7
Oladipo <phaymos2022@gmail.com>
2023-05-08 12:30:28 -0700

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,79 @@
+#include "main.h"
+void cw_err(int fd, char *av);
+void open_err(int fd, char *av);
+/**
+ * main - copies a file to another
+ * @ac: number
+ * @av: pointer to args
+ *
+ * Return: 0
+ */
+int main(int ac, char *av[])
+{
+int fd;
+ssize_t check;
+ssize_t w_check;
+char *buff = malloc(sizeof(char *) * 1000);
+if (ac != 3)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+fd =  open(av[1], O_RDONLY);
+open_err(fd, av[1]);
+check = read(fd, buff, 4000);
+if (check == -1)
+	close(fd);
+open_err(check, av[1]);
+close(fd);
+fd = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+if (fd == -1)
+	close(fd);
+cw_err(fd, av[2]);
+while (check > 0)
+{
+	w_check = write(fd, buff, 1024);
+	if (w_check == -1)
+		close(fd);
+	cw_err(w_check, av[2]);
+	check -= w_check;
+	buff += w_check;
+}
+close(fd);
+return (0);
+}
+
+/**
+ * open_err - open error
+ * @fd: des
+ * @av: name
+ *
+ * Return: void
+ */
+void open_err(int fd, char *av)
+{
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av);
+exit(98);
+}
+else
+return;
+}
+/**
+ * cw_err - creatw write erroe
+ * @fd: des
+ * @av: name
+ *
+ * Return: void
+ */
+void cw_err(int fd, char *av)
+{
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av);
+exit(99);
+}
+else
+return;
+}
