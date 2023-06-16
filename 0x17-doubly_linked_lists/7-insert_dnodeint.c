3f9c5772cd985c5e480a3f13188cee5d8e480fc4
oladipo <phaymos2022@gmail.com>
2023-06-16 01:52:44 +0100

    changes the rrturn statement to NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	if (temp == NULL)
-		return (0);
+		return (NULL);
 	temp->n = n;
 	if (idx == 0)
 	{
