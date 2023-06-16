c47397337e8aeba1809c857451680d46079c98be
oladipo <phaymos2022@gmail.com>
2023-06-16 17:05:53 +0100

    removed prev statement;

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		temp2 = temp2->next;
 		*head = temp2;
-		temp2->prev = NULL;
+		/*temp2->prev = NULL;*/
 		return (1);
 	}
 	while (i < index - 1)
