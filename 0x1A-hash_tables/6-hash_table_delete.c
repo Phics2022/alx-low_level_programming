cfaeda568e7da828d58285d0914ea590120f086b
oladipo <phaymos2022@gmail.com>
2023-07-09 00:15:28 +0100

    did task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *item;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		item = ht->array[i];
+
+		if (item != NULL)
+		{
+			free(item->key);
+			free(item->value);
+			free(item);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
