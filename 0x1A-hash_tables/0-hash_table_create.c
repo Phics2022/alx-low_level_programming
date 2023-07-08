e7fb8ed14e1d542c17f2a7765b62ca5e7e120de0
oladipo <phaymos2022@gmail.com>
2023-07-08 21:17:50 +0100

    added a conditional

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
+	if (new_table == NULL)
+		return (NULL);
 	new_table->size = size;
 	/* realloc size of the node */
 	new_table->array = calloc(new_table->size, sizeof(hash_node_t *));
