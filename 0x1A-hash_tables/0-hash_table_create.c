9fa8833fedb33aa53b3fa035506fe078a6e80b82
oladipo <phaymos2022@gmail.com>
2023-09-07 19:21:46 +0100

    added a new conditional statement

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 	hash_table_t *new_table;
 
+	if (size == 0)
+		return (NULL);
 	/* creating memory for the hash table */
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
